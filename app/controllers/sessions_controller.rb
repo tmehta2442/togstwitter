560143d6456d097fcbca5d1173652aca59f5d879
tmehta2442 <tmehta@post.harvard.edu>
2013-12-24 13:18:20 -0800

    controller not working right, tests

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,16 @@ class SessionsController < ApplicationController
 	end
 
 	def create
+		user = User.find_by_email(params[:session][:email])
+		if user && user.authentication(params[:session][:password])
+
+		else
+			flash[:error] = 'Invalid email/password combination'
+			render 'new'
+		end
 	end
 
 	def destroy
 	end
-	
+
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -19,6 +19,11 @@ describe "Authentication" do
 
 			it { should have_selector('title', text: 'Sign in') }
 			it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+
+			describe "after visiting another page" do
+				before { click_link "home" }
+				it { should_not have_selector('div.alter.alert-error') }
+			end
 		end
 
 		describe "with valid information" do
