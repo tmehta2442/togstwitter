64669212907b7279a3a5eff5db859d5bfe3cd00d
tmehta2442 <tmehta@post.harvard.edu>
2013-12-23 16:26:17 -0800

    finished user signup

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -34,7 +34,7 @@ describe "User pages" do
 				fill_in "Name", with: "Example User"
 				fill_in "Email", with: "user@example.com"
 				fill_in "Password", with: "foobar"
-				fill_in "confirmation", with: "foobar"
+				fill_in "Confirmation", with: "foobar"
 			end
 
 
