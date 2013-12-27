80f81354483f9cd1b11309f6c3d871e1a5ac03fc
tmehta2442 <tmehta@post.harvard.edu>
2013-12-26 23:46:30 -0800

    access control

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -66,13 +66,8 @@ describe "Authentication" do
 					end
 				end
 			end
-		end
-
-		describe "for non-signed-in users" do
-			let(:user) { FactoryGirl.create(:user) }
 
 			describe "in the Users controller" do
-
 				describe "visiting the edit page" do
 					before { visit edit_user_path(user) }
 					it { should have_selector('title', text: 'Sign in') }
@@ -88,6 +83,22 @@ describe "Authentication" do
 					it { should have_selector('title', text: 'Sign in') }
 				end
 			end
+
+			describe "in the Microposts controller" do
+				describe "submitting to the create action" do
+					before { post microposts_path }
+					specify { response.should redirect_to(signin_path) }
+				end
+
+				describe "submitting to the destroy action" do
+					before do
+						micropost = FactoryGirl.create(:micropost)
+						delete micropost_path(micropost)
+					end
+					specify { response.should redirect_to(signin_path) }
+				end
+			end
+
 		end
 
 		describe "as wrong user" do
