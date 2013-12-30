97444f48bdda976ff8410c9d22c5addc48f8b71f
tmehta2442 <tmehta@post.harvard.edu>
2013-12-29 22:47:36 -0800

    starting password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Togstwitter::Application.routes.draw do
+  get "password_resets/new"
+
   resources :users do
     member do
       get :following, :followers
@@ -8,7 +10,8 @@ Togstwitter::Application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   resources :microposts, only: [:create, :destroy]
   resources :relationships, only: [:create, :destroy]
-
+  resources :password_resets
+  
   root to: 'static_pages#home'
 
   match '/signup', 	to: 'users#new'
